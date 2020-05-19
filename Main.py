642e8fc5e3fd7320bc7a561b0fbb894a22ebc057
Dannana Venkataramana <dvenkat507@gmail.com>
2020-05-19 10:56:54 +0530

    Update

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ import Configuration as conf
 
 cf.createDirectory()
 
-#for i in conf.File_Names:
-   #cf.Ping_Parser(conf.Input_File_Directory + '/' + i, conf.Log_Directory + '/' + conf.Output_Files + '/' + i)
+for i in conf.File_Names:
+   cf.Ping_Parser(conf.Input_File_Directory + '/' + i, conf.Log_Directory + '/' + conf.Output_Files + '/' + i)
 
-#for i in conf.File_Names:
-    #cf.Latency_vs_Time(conf.Input_File_Directory + '/' + i, conf.Log_Directory + '/' + conf.Image_Directory + '/' + i.split('.')[0] + '.png')
+for i in conf.File_Names:
+    cf.Latency_vs_Time(conf.Input_File_Directory + '/' + i, conf.Log_Directory + '/' + conf.Image_Directory + '/' + i.split('.')[0] + '.png')
 
 for i in conf.File_Names:
     cf.Histogram_Plot(conf.Log_Directory + '/' + conf.Output_Files + '/' + i, conf.Log_Directory + '/' + conf.Histogram_Plot + '/' + i, conf.Log_Directory + '/' + conf.Histogram_Plot + '/' + i.split('.')[0] + '.png')
