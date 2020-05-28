e96e9b8234ce3bc8db79f6288e2c52d1ff114b3c
Dannana Venkataramana <vdannana@netgear.com>
2020-05-28 11:54:55 +0530

    Update

--- Configuration.py
+++ Configuration.py
@@ -1,8 +1,23 @@
+# list of input file name.
 File_Names = ["Google.txt", 'GW.txt', 'Router_Eth_Banckedn.txt', 'SAT1_eth_backend.txt', 'Yahoo.txt']
+
+# Number of consecutive ping drop to consider.
 Max_Count = 4
+
+# Input file directory.
 Input_File_Directory = 'Input'
+
+# Output log directory.
 Log_Directory = "Build_Version"
+
+# Graphs Ouput directory
 Image_Directory = "Graphs"
+
+# Output parsed log directory.
 Output_Files = "Output_Files"
+
+# Histogram graph output directory.
 Histogram_Plot = "Histogram_Plot"
+
+# Bucket Size.
 Bucket_size = 10
--- Main.py
+++ Main.py
@@ -2,13 +2,17 @@
 import Common_Function as cf
 import Configuration as conf
 
+# create required directory.
 cf.createDirectory()
 
+# Parses ping files and fetches consecutive ping drops records.
 for i in conf.File_Names:
    cf.Ping_Parser(conf.Input_File_Directory + '/' + i, conf.Log_Directory + '/' + conf.Output_Files + '/' + i)
 
+# parses ping files and plot latency v/s time graph.
 for i in conf.File_Names:
     cf.Latency_vs_Time(conf.Input_File_Directory + '/' + i, conf.Log_Directory + '/' + conf.Image_Directory + '/' + i.split('.')[0] + '.png')
 
+#parses output of Ping_Parser function and plot histogram.
 for i in conf.File_Names:
     cf.Histogram_Plot(conf.Log_Directory + '/' + conf.Output_Files + '/' + i, conf.Log_Directory + '/' + conf.Histogram_Plot + '/' + i, conf.Log_Directory + '/' + conf.Histogram_Plot + '/' + i.split('.')[0] + '.png')
